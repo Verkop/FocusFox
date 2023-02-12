f9746d0e33a59961fccd30ddb99344b1227d6acc
Nannanas <32544459+Verkop@users.noreply.github.com>
2023-02-12 20:23:10 +0100

    Inital project setup (#1)

---
+++ src/background.ts
@@ -0,0 +1,11 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.onUpdated.addListener(
+    () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+        if (id) {
+          chrome.action.disable(id)
+        }
+      })
+    }
+  )
+})
