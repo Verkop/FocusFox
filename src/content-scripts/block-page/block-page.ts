ca2610d56e159c8cfbc250c7c3510755ffe72249
Nannanas <32544459+vekopf@users.noreply.github.com>
2023-02-23 18:35:48 +0100

    Add base class for content script pages (#8)

--- src/content-scripts/block-page/block-page.ts
+++ src/content-scripts/block-page/block-page.ts
@@ -1,25 +1,11 @@
-import { switchMap, take } from 'rxjs'
-import Browser from 'src/browser/browser'
-import Document from 'src/document/document'
+import ContentScriptPage from 'src/content-scripts/content-script-page'
 
-// The document body must be hidden before the page is loaded, otherwise the original page is visible for a moment before it is replaced by the block page
-Document.hideBody()
+export default class BlockPage extends ContentScriptPage {
+  public static id = 'block-page'
 
-Document.setFont('Quicksand', 'assets/fonts/Quicksand-Regular.ttf')
-Browser.getFileContent('content-scripts/css/block-page.css')
-  .pipe(take(1))
-  .subscribe((style) => {
-    console.log(style)
-    Document.addStyle(style)
-  })
+  constructor() {
+    super(BlockPage.id)
+  }
+}
 
-// The body must be replaced after the original document has been loaded otherwise the original page overwrites the block page as soon as it is loaded
-Document.contentLoaded
-  .pipe(
-    take(1),
-    switchMap(() => Browser.getFileContent('content-scripts/html/block-page.html'))
-  )
-  .subscribe((pageContent) => {
-    Document.replaceBody(pageContent)
-    Document.showBody()
-  })
+new BlockPage().show()
